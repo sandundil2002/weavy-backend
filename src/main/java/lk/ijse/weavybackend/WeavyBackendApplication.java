f37a5eae5d9d18d97cd0c098cfba1d459974e605
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2025-04-04 09:09:00 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/weavybackend/WeavyBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.weavybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeavyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeavyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/weavybackend/WeavyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.weavybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeavyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
